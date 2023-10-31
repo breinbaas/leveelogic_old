ee53ece9585d6bc17fa2845a3ca2eed954b18b7a
Rob van Putten <breinbaasnl@gmail.com>
2023-10-31 09:22:30 +0100

    easier input

---
+++ leveelogic/deltares/algorithms/__init__.py
@@ -0,0 +1,6 @@
+from algorithm_berm import *
+from algorithm_excavation import *
+from algorithm_mirror import *
+from algorithm_move import *
+from algorithm_phreatic_line import *
+from algorithm_tree import *
