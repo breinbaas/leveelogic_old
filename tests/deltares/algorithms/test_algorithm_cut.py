e4f11ecab2f113776a319d133998538d221e658c
Rob van Putten <breinbaasnl@gmail.com>
2024-02-07 15:56:14 +0100

    added extra test

--- tests/deltares/algorithms/test_algorithm_cut.py
+++ tests/deltares/algorithms/test_algorithm_cut.py
@@ -8,3 +8,9 @@ class TestAlgorithmCut:
         alg = AlgorithmCut(ds=ds, points=[(0, 3), (13, 3), (25, -11), (55, -11)])
         ds = alg.execute()
         ds.serialize("tests/testdata/output/simple_geometry_cut.stix")
+
+    def test_complex_geometry(self):
+        ds = DStability.from_stix("tests/testdata/stix/fc_alg_pl_wsbd.stix")
+        alg = AlgorithmCut(ds=ds, points=[(-100, 3), (40, -1.5), (130.496, -1.5)])
+        ds = alg.execute()
+        ds.serialize("tests/testdata/output/fc_alg_pl_wsbd_cut.stix")
